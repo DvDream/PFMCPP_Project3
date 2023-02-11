82e173258893a1c955785826b49b160d7da668b0
Davide Mori <45291289+DvDream@users.noreply.github.com>
2023-02-11 17:16:24 +0000

    Part 1c - Step 4: done assignment

--- main.cpp
+++ main.cpp
@@ -337,17 +337,17 @@ Define an object that is made of 5 sub-objects.
     These 5 sub-objects will not be defined using Primitives, but instead will be their own UDTs 
     you'll define these 5 sub-objects in Part 1d.
 
-Thing 10)
+Thing 10) Scooter
 5 properties:
-    1)
-    2)
-    3)
-    4)
-    5)
+    1) HandleBar
+    2) Wheel
+    3) Seat
+    4) Light
+    5) Case
 3 things it can do:
-    1)
-    2)
-    3)
+    1) accelerate
+    2) brake
+    3) steer
  */
 
  /*
